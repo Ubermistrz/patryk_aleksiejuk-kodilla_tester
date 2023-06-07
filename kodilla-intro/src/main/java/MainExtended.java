09e82a0efa4c5fe1c65caffb21978df13e804109
Patryk Aleksiejuk <aleksiejukpatryk@gmail.com>
2023-06-07 13:37:28 +0200

    Test

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,20 @@
+public class MainExtended {
+    public static void main(String[] args){
+        String welcome = "Hi im Patryk";
+        String hobby = "Games";
+        int myAge = 21;
+        double height = 1.69;
+        char firstLetterOfLastName = 'A';
+        boolean likesanimal = true;
+
+        System.out.println(welcome);
+        System.out.println(hobby);
+        System.out.println(myAge);
+        System.out.println(height);
+        System.out.println(firstLetterOfLastName);
+        System.out.println(likesanimal);
+
+
+
+    }
+}
