f77dbcfa2347be11309ded121148a434be00c514
Patryk Aleksiejuk <aleksiejukpatryk@gmail.com>
2023-06-07 13:30:34 +0200

    Test

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld!");            //[1]
+        System.out.println("Patryk");                 //[2]
+        System.out.println(21);                       //[3]
+        System.out.println('A');                      //[4]
+    }
+}
